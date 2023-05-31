18c6a43d54162d31c80d2c7547a30a5428192a94
LeeSeungJun <cjstkrak@gmail.com>
2023-06-01 02:33:33 +0900

    평점정렬추가

--- app3.js
+++ app3.js
@@ -35,7 +35,6 @@ async function renderMovies() {  /* 패치 된 내용을 찍는 과정 */
         // div에 onclick=alert(${x.id}) 를 바로 할당했다. 안될줄 알았다. 다른 함수를 새로 생성해 onclick 에 부여해보기
         html += htmlSegment;
     });
-
     let container = document.querySelector('#cards-box');
     container.innerHTML = html;
 }
@@ -53,12 +52,12 @@ async function serch() { /* 검색으로 새로 불러오는 데이터 */
     const serchData = movies.filter((x) => {
         let a = x.title.toUpperCase()
         return a.includes(inputtext)
-    })
+    }) // indexOf 는 안된다
 
     serchData.forEach((x) => {
         let htmlSegment = `
-        <div class="card h-100">
-            <img onclick="alert(${x.id})" src="https://image.tmdb.org/t/p/w500${x.poster_path}"
+        <div onclick="alert(${x.id})" class="card h-100">
+            <img src="https://image.tmdb.org/t/p/w500${x.poster_path}"
                 class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">${x.title}</h5>
@@ -74,7 +73,7 @@ async function serch() { /* 검색으로 새로 불러오는 데이터 */
     container.innerHTML = html;
 }
 
-async function sort() {
+async function sort_title() {
     let { results: movies } = await getMovies();
     const sort_data = movies.sort((a, b) => a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1);
 
@@ -82,8 +81,67 @@ async function sort() {
 
     sort_data.forEach((x) => {
         let htmlSegment = `
-        <div class="card h-100">
-            <img onclick="alert(${x.id})" src="https://image.tmdb.org/t/p/w500${x.poster_path}"
+        <div onclick="alert(${x.id})" class="card h-100">
+            <img src="https://image.tmdb.org/t/p/w500${x.poster_path}"
+                class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${x.title}</h5>
+                <p class="card-text">${x.overview}</p>
+                <p>평점 : ${x.vote_average}</p>
+            </div>
+        </div>
+    `;
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('#cards-box');
+    container.innerHTML = html;
+}
+
+async function sort_date() {
+    let { results: movies } = await getMovies();
+    const sort_data = movies.sort((a, b) => a.release_date > b.release_date ? -1 : 1);
+
+    let html = '';
+
+    sort_data.forEach((x) => {
+        let htmlSegment = `
+        <div onclick="alert(${x.id})" class="card h-100">
+            <img src="https://image.tmdb.org/t/p/w500${x.poster_path}"
+                class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${x.title}</h5>
+                <p class="card-text">${x.overview}</p>
+                <p>평점 : ${x.vote_average}</p>
+            </div>
+        </div>
+    `;
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('#cards-box');
+    container.innerHTML = html;
+}
+
+async function sort_vote() {
+    let { results: movies } = await getMovies();
+    const sort_data = movies.sort((a, b) => {
+        if(a.vote_average > b.vote_average) 
+        return -1  
+        else if(a.vote_average < b.vote_average) 
+        return 1
+        else if(a.vote_count > b.vote_count)
+        return -1
+        else if(a.vote_count < b.vote_count)
+        return 1
+});
+
+    let html = '';
+
+    sort_data.forEach((x) => {
+        let htmlSegment = `
+        <div onclick="alert(${x.id})" class="card h-100">
+            <img src="https://image.tmdb.org/t/p/w500${x.poster_path}"
                 class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">${x.title}</h5>
