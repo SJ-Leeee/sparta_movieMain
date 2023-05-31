2e8d9cf997e02d7af820335f999b4ff658ed8e6c
LeeSeungJun <cjstkrak@gmail.com>
2023-05-31 19:53:55 +0900

    이름정렬기능 추가

--- app3.js
+++ app3.js
@@ -17,25 +17,22 @@ async function getMovies() { // fetch된 파일을 객체를 가져오려면 the
 // async function getMoviesdata() {
 //     return await getMovies() } // 바로쓰면 되는데 왜 리턴하는가? 안만들고 바로 할당가능 일단 주석
 
-
-
 async function renderMovies() {  /* 패치 된 내용을 찍는 과정 */
     let { results: movies } = await getMovies(); // 객체구조분해할당 방식으로 변수저장을 해줘야한다. getMovie()로 가져온 데이터는 배열이기 때문에 객체로 변환
     // let { logo_sizes: posters } = await getPostersdata();
-    let html = ''; 
+    let html = '';
     movies.forEach((x) => {
-        let htmlSegment = `<div class="col">
-        <div class="card h-100">
-            <img onclick="alert(${x.id})" src="https://image.tmdb.org/t/p/w500${x.poster_path}"
+        let htmlSegment = `
+        <div onclick="alert(${x.id})" class="card h-100">
+            <img src="https://image.tmdb.org/t/p/w500${x.poster_path}"
                 class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">${x.title}</h5>
                 <p class="card-text">${x.overview}</p>
                 <p class="average">평점 : ${x.vote_average}</p>
             </div>
-        </div>
-    </div>`;
-        // img onclick 에 alert(${x.id}) 를 바로 할당했다. 안될줄 알았다. 다른 함수를 새로 생성해 onclick 에 부여해보기
+        </div>`
+        // div에 onclick=alert(${x.id}) 를 바로 할당했다. 안될줄 알았다. 다른 함수를 새로 생성해 onclick 에 부여해보기
         html += htmlSegment;
     });
 
@@ -47,27 +44,54 @@ async function renderMovies() {  /* 패치 된 내용을 찍는 과정 */
 
 renderMovies(); // 바로 실행 시켜 줌 으로써 html에 바로 출력
 
-async function serch() { /* 검색으로 새로 불러오는 데이터 (진행중) */
-    var value
+async function serch() { /* 검색으로 새로 불러오는 데이터 */
+
+    let { results: movies } = await getMovies(); // 객체구조분해할당 방식으로 변수저장을 해줘야한다. getMovie()로 가져온 데이터는 배열이기 때문에 객체로 변환
+    let inputtext
     let html = '';
-    value = document.getElementById("serch").value;
-    let { results: movies } = await getMovies();
+    inputtext = document.getElementById("serch").value.toUpperCase();
     const serchData = movies.filter((x) => {
-        return x.title.includes(value)
+        let a = x.title.toUpperCase()
+        return a.includes(inputtext)
     })
 
     serchData.forEach((x) => {
-        let htmlSegment = `<div class="col">
+        let htmlSegment = `
         <div class="card h-100">
             <img onclick="alert(${x.id})" src="https://image.tmdb.org/t/p/w500${x.poster_path}"
                 class="card-img-top">
             <div class="card-body">
                 <h5 class="card-title">${x.title}</h5>
                 <p class="card-text">${x.overview}</p>
-                <p class="average">평점 : ${x.vote_average}</p>
+                <p>평점 : ${x.vote_average}</p>
             </div>
         </div>
-    </div>`;
+    `;
+        html += htmlSegment;
+    });
+
+    let container = document.querySelector('#cards-box');
+    container.innerHTML = html;
+}
+
+async function sort() {
+    let { results: movies } = await getMovies();
+    const sort_data = movies.sort((a, b) => a.title.toLowerCase() < b.title.toLowerCase() ? -1 : 1);
+
+    let html = '';
+
+    sort_data.forEach((x) => {
+        let htmlSegment = `
+        <div class="card h-100">
+            <img onclick="alert(${x.id})" src="https://image.tmdb.org/t/p/w500${x.poster_path}"
+                class="card-img-top">
+            <div class="card-body">
+                <h5 class="card-title">${x.title}</h5>
+                <p class="card-text">${x.overview}</p>
+                <p>평점 : ${x.vote_average}</p>
+            </div>
+        </div>
+    `;
         html += htmlSegment;
     });
 
